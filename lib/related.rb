375b79375ef9b93f74fab86d8ba321040f9a0de0
Vladimir Yarotsky <v.yarotsky@itransition.com>
2013-03-30 22:57:12 +0300

    Version info

--- lib/related.rb
+++ lib/related.rb
@@ -8,6 +8,9 @@ require 'related/frameworks'
 module Related
   extend self
 
+  version_file = File.expand_path('../VERSION', File.dirname(__FILE__))
+  VERSION = File.read(version_file).chomp.freeze
+
   def open_related_file
     framework.open_related_file
   end
