ab3c9475cb8ee058c9782a8e41d4f300d188a909
Vladimir Yarotsky <v.yarotsky@itransition.com>
2013-08-19 10:42:39 +0300

    Add create file prompt

--- lib/related.rb
+++ lib/related.rb
@@ -12,13 +12,19 @@ module Related
   version_file = File.expand_path('../VERSION', File.dirname(__FILE__))
   VERSION = File.read(version_file).chomp.freeze
 
+  # Relying on cache-absence here
+  # If user creates a file with different name in #ensure_file!
+  # the repo is scanned again and the custom file is likely found
+  #
   def open_related_file
+    ensure_file!(framework.related_file)
     VIM.command "silent :e #{framework.related_file}"
   rescue RelatedError => e
     VIM.message e.message
   end
 
   def run_test
+    ensure_file!(framework.test_file)
     @command = "clear && cd #{related_paths.repo_root} && #{framework.run_test_command}"
     runner.run(@command)
   rescue RelatedError => e
@@ -32,11 +38,23 @@ module Related
   private
 
   def framework
-    Frameworks.detect(related_paths)
+    @framework ||= Frameworks.detect(related_paths)
   end
 
   def related_paths
     @related_paths ||= RelatedPaths.new
+  end
+
+  # TODO move and test
+  def ensure_file!(file)
+    return if File.exists?(file)
+    if VIM.evaluate(%{confirm("Create #{file}?", "&Yes\n&No", 1)}) == 1
+      file_to_create = VIM.evaluate(%{input("File to create: ", "#{file}")})
+      Pathname.new(file_to_create).dirname.mkpath
+      File.open(file_to_create, "w") {}
+    else
+      raise RelatedError, "File #{file} does not exist"
+    end
   end
 
   def runner
